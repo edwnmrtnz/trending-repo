2ecc05f1c751ec552b3d9a12e3c66e29283ff398
Edwin Martinez <dev.edwinmartinez@gmail.com>
2023-06-06 13:07:38 +0800

    Test throwing Trendy based exception

--- app/src/test/java/com/edwnmrtnz/trendingrepo/core/data/DefaultGithubReposRepositoryTest.kt
+++ app/src/test/java/com/edwnmrtnz/trendingrepo/core/data/DefaultGithubReposRepositoryTest.kt
@@ -2,10 +2,15 @@ package com.edwnmrtnz.trendingrepo.core.data
 
 import com.edwnmrtnz.trendingrepo.AppTestConstant
 import com.edwnmrtnz.trendingrepo.TestAssetReader
+import com.edwnmrtnz.trendingrepo.core.domain.exceptions.TrendyHttpErrorException
+import com.edwnmrtnz.trendingrepo.core.domain.exceptions.TrendyServiceFailureException
 import com.google.common.truth.Truth
+import java.nio.Buffer
 import kotlinx.coroutines.test.runTest
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.SocketPolicy
+import okio.ByteString
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -47,5 +52,25 @@ class DefaultGithubReposRepositoryTest {
         val result = sut.load()
 
         Truth.assertThat(result).isNotEmpty()
+    }
+
+    @Test(expected = TrendyHttpErrorException::class)
+    fun `should be able to throw trendy http exception when api failed on load`() = runTest {
+        val response = MockResponse()
+            .setResponseCode(404)
+            .setBody("{}")
+        server.enqueue(response)
+
+        sut.load()
+    }
+
+    @Test(expected = TrendyServiceFailureException::class)
+    fun `should be able to throw trendy service exception when no network on load`() = runTest {
+        val response = MockResponse()
+            .setBody(okio.Buffer().write(byteString = ByteString.EMPTY))
+            .setSocketPolicy(SocketPolicy.DISCONNECT_DURING_RESPONSE_BODY)
+        server.enqueue(response)
+
+        sut.load()
     }
 }
