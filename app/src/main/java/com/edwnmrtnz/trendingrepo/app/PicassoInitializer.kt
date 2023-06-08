6aa5b17f4fac79cfed5df70cc9c5637a9ff140da
Edwin Martinez <dev.edwinmartinez@gmail.com>
2023-06-08 22:15:09 +0800

    Images does not load on Pie devices and above because of ClearTextTraffic

--- app/src/main/java/com/edwnmrtnz/trendingrepo/app/PicassoInitializer.kt
+++ app/src/main/java/com/edwnmrtnz/trendingrepo/app/PicassoInitializer.kt
@@ -9,10 +9,12 @@ object PicassoInitializer {
     fun init(context: Context) {
         val picasso = Picasso.Builder(context.applicationContext)
             .listener { _, uri, exception ->
-                Log.e("Picasso", "Failed to load $uri because $exception")
+                Log.e("Picasso", "Failed to load $uri > ${exception.message}")
             }
+            .loggingEnabled(BuildConfig.DEBUG)
             .indicatorsEnabled(BuildConfig.DEBUG)
             .build()
+
         Picasso.setSingletonInstance(picasso)
     }
 }
